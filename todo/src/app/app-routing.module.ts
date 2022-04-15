1a3c5bcdbbb17780824c5266efd2598dde16edb4
Paulo Previlato <paulo.previlato@gmail.com>
2022-04-15 17:17:39 -0400

    create router

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { FinalizadosComponent } from './components/finalizados/finalizados.component';
+import { ReadAllComponent } from './components/read-all/read-all.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { finalize } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ReadAllComponent
+  },
+  {
+    path: 'FinalizadosComponent',
+    component: FinalizadosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
