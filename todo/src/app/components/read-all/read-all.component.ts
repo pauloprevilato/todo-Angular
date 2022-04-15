7082469720b7781417f9806291e81789d7f2c800
Paulo Previlato <paulo.previlato@gmail.com>
2022-04-15 10:54:46 -0400

    create read-all

--- todo/src/app/components/read-all/read-all.component.ts
+++ todo/src/app/components/read-all/read-all.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-read-all',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./read-all.component.css']
 })
 export class ReadAllComponent implements OnInit {
+
+  list: Todo [] = [
+    {
+      titulo: "Aula de Spring",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    },
+    {
+      titulo: "Aula de Angular",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    }
+  ]
 
   constructor() { }
 
